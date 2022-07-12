16b7ecf405d9ba8a1afa333bff1dad3667a5ffa3
Daxin <wangxin@gekj.net>
2022-07-12 20:30:09 +0800

    初始化.13KV334444444545

--- app/Http/Controllers/Api/KOOKController.php
+++ app/Http/Controllers/Api/KOOKController.php
@@ -44,7 +44,8 @@ class KOOKController extends Controller
         Log::channel('mylog')->info('0.>>> | '.oToJson($challenge));
 
         return response()->json([ // 构造 webHook 回调验证
-            'challenge' => $challenge
+            'challenge' => $challenge,
+            'code' => $challenge
         ]);
     }
 }
