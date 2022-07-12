80219274bb6a20380062f402b8a6fbfe6b767fec
Daxin <wangxin@gekj.net>
2022-07-12 19:54:20 +0800

    初始化.13KV33444444

--- app/Http/Controllers/Api/KOOKController.php
+++ app/Http/Controllers/Api/KOOKController.php
@@ -22,25 +22,25 @@ class KOOKController extends Controller
         $data = $Bot['d'] ?? null; // 赋值 $post['d'] 下的成员数据 到 $data
         Log::channel('mylog')->info('3.>>> | '.oToJson($data));
         $challenge = $data['challenge'] ?? null; // 赋值 接收并响应事件 应用需要原样返回的值
-        $channel_type = $data['channel_type'] ?? null; // 赋值 消息频道类型 GROUP 为频道消息 PERSON 为个人消息为
-        $event_type = $data['extra']['type'] ?? null; // 接收事件类型
-        if ($sn == 0){ // 判定 事件唯一ID 为0时初始化
-            $kv->set_data($dbSn, $sn);
-        }
-        if ($sn == $oSn) {
-            return json_encode([ // 构造 webHook 回调验证
-                'challenge' => $challenge
-            ]);
-        } else {
-            // 获取事件唯一ID 并记录ID到本地
-            $kv->set_data($dbSn, $sn);
-        };
-
-        if ($robot == 'gaoxueya'){
-            if ($event_type =='' && $channel_type == 'PERSON'){
-
-            }
-        }
+//        $channel_type = $data['channel_type'] ?? null; // 赋值 消息频道类型 GROUP 为频道消息 PERSON 为个人消息为
+//        $event_type = $data['extra']['type'] ?? null; // 接收事件类型
+//        if ($sn == 0){ // 判定 事件唯一ID 为0时初始化
+//            $kv->set_data($dbSn, $sn);
+//        }
+//        if ($sn == $oSn) {
+//            return json_encode([ // 构造 webHook 回调验证
+//                'challenge' => $challenge
+//            ]);
+//        } else {
+//            // 获取事件唯一ID 并记录ID到本地
+//            $kv->set_data($dbSn, $sn);
+//        };
+//
+//        if ($robot == 'gaoxueya'){
+//            if ($event_type =='' && $channel_type == 'PERSON'){
+//
+//            }
+//        }
         return json_encode([ // 构造 webHook 回调验证
             'challenge' => $challenge
         ]);
