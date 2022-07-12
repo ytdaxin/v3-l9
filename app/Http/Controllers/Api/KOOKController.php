310b6528cd7622c4848998c43072e461ff496afe
Daxin <wangxin@gekj.net>
2022-07-12 19:53:07 +0800

    初始化.13KV3344444

--- app/Http/Controllers/Api/KOOKController.php
+++ app/Http/Controllers/Api/KOOKController.php
@@ -15,10 +15,12 @@ class KOOKController extends Controller
         $kv = new ReplitDB();
         $dbSn = 'webHook_gxy_sn'; //事件唯一ID
         $sn = $Bot['sn'] ?? 0;
+        Log::channel('mylog')->info('1.>>> | '.oToJson($sn).oToJson($Bot));
         $oSn = $kv->get_data($dbSn); // 取回 事件唯一ID
+        Log::channel('mylog')->info('2.>>> | '.oToJson($oSn));
         $robot = $Bot['robot'] ?? null;
         $data = $Bot['d'] ?? null; // 赋值 $post['d'] 下的成员数据 到 $data
-        Log::channel('mylog')->info('>>> | '.oToJson($data));
+        Log::channel('mylog')->info('3.>>> | '.oToJson($data));
         $challenge = $data['challenge'] ?? null; // 赋值 接收并响应事件 应用需要原样返回的值
         $channel_type = $data['channel_type'] ?? null; // 赋值 消息频道类型 GROUP 为频道消息 PERSON 为个人消息为
         $event_type = $data['extra']['type'] ?? null; // 接收事件类型
