ae040c2b60cc2525afe74ae76564538cc2bccb5b
远拓大鑫 <ytall@qq.com>
2022-12-01 00:43:22 +0800

    初始化.11012123

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -119,6 +119,10 @@
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
                                 Sponsor
                             </a>
+
+                            <a href="http://www.leyoui.com" class="ml-1 underline">
+                                线上测试
+                            </a>
                         </div>
                     </div>
 
