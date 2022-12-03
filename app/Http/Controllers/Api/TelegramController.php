c9617d598110e356ec22a9068a9dc4f26c91d52f
远拓大鑫 <ytall@qq.com>
2022-12-03 14:26:04 +0800

    Telegram 方法1

--- app/Http/Controllers/Api/TelegramController.php
+++ app/Http/Controllers/Api/TelegramController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -15,7 +16,23 @@ class TelegramController extends Controller
             Log::channel('syslog')->info('code:20001,消息格式错误 | ' . oToJson($res));
             return "code:20001,消息格式错误".json_encode($res);
         }
+        $this->_msgSave(json_encode($res));
         Log::channel('syslog')->info(oToJson($res));
         return true;
+    }
+
+    public function _msgSave(string $data)
+    {
+        $http = new Client([
+            'base_uri' => 'http://bb.ziyouyu.cn/api/v1/',
+            'headers'   =>  [
+                'content-type'  =>  'application/json'
+            ]
+        ]);
+        $params = [
+            'TgData' => $data
+        ];
+        $res = $http->post('Telegram', ['form_params' => $params]);
+        return;
     }
 }
