a966b2dff02f1562d79a5a0f81f27ea3f964fcf5
远拓大鑫 <ytall@qq.com>
2022-12-03 22:37:03 +0800

    Telegram 方法1234345

--- app/Http/Controllers/Api/TelegramController.php
+++ app/Http/Controllers/Api/TelegramController.php
@@ -13,11 +13,9 @@ class TelegramController extends Controller
     {
         $res = $request->all();
 //        if(!isset($res['update_id'])){
-//            Log::channel('syslog')->info('code:20001,消息格式错误 | ' . oToJson($res));
 //            return "code:20001,消息格式错误".json_encode($res);
 //        }
         $this->_msgSave(json_encode($res));
-        Log::channel('syslog')->info(oToJson($res));
         return true;
     }
 
