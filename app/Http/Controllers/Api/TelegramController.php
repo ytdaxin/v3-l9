c372aa4100e0d4140c18551f7fe7b80278b49f6f
远拓大鑫 <ytall@qq.com>
2022-12-04 10:41:09 +0800

    Telegram 方法883

--- app/Http/Controllers/Api/TelegramController.php
+++ app/Http/Controllers/Api/TelegramController.php
@@ -18,43 +18,44 @@ class TelegramController extends Controller
             return $this->_msgSave("code:20001,消息格式错误".json_encode($info));
         }
         Log::channel('mylog')->info(oToJson($info));
-        if (isset($info['message']['chat']['type']) && $info['message']['chat']['type'] == 'private'){ //私聊
-
-            if (isset($info['message']['text'])){ //文本
-                $this->_sendMsg([
-                    'chat_id' => $info['message']['chat']['id'],
-                    'text' => $info['message']['text'],
-                ]);
-            }else if(isset($info['message']['photo'])){ //图片
-
-            }else{
-                return "未识别私聊";
-            }
-        }else if(isset($info['message']['chat']['type']) && $info['message']['chat']['type'] == 'supergroup'){ //群组
-            if (isset($info['message']['text'])){ //文本
-                $this->_sendMsg([
-                    'chat_id' => $info['message']['chat']['id'],
-                    'text' => $info['message']['text'],
-                ]);
-            }else if(isset($info['sticker'])){ //表情
-
-            }else{
-                return "未识别群组";
-            }
-        }else if(isset($info['channel_post']['chat']['type']) && $info['message']['chat']['type'] == 'channel'){ //频道
-            if (isset($info['channel_post']['text'])){ //文本
-                $this->_sendMsg([
-                    'chat_id' => $info['message']['chat']['id'],
-                    'text' => $info['message']['text'],
-                ]);
-            }else if(isset($info['channel_post']['photo'])){ //图片
-
-            }else{
-                return "未识别频道";
-            }
-        }else{
-            return "未识别频道";
-        }
+//        if (isset($info['message']['chat']['type']) && $info['message']['chat']['type'] == 'private'){ //私聊
+//
+//            if (isset($info['message']['text'])){ //文本
+//                $this->_send_msg([
+//                    'chat_id' => $info['message']['chat']['id'],
+//                    'text' => $info['message']['text'],
+//                ]);
+//            }else if(isset($info['message']['photo'])){ //图片
+//
+//            }else{
+//                return "未识别私聊";
+//            }
+//        }else if(isset($info['message']['chat']['type']) && $info['message']['chat']['type'] == 'supergroup'){ //群组
+//            if (isset($info['message']['text'])){ //文本
+//                $this->_send_msg([
+//                    'chat_id' => $info['message']['chat']['id'],
+//                    'text' => $info['message']['text'],
+//                ]);
+//            }else if(isset($info['sticker'])){ //表情
+//
+//            }else{
+//                return "未识别群组";
+//            }
+//        }else if(isset($info['channel_post']['chat']['type']) && $info['message']['chat']['type'] == 'channel'){ //频道
+//            if (isset($info['channel_post']['text'])){ //文本
+//                $this->_send_msg([
+//                    'chat_id' => $info['message']['chat']['id'],
+//                    'text' => $info['message']['text'],
+//                ]);
+//            }else if(isset($info['channel_post']['photo'])){ //图片
+//
+//            }else{
+//                return "未识别频道";
+//            }
+//        }else{
+//            return "未识别频道";
+//        }
+        $this->_msgSave($info);
         return true;
     }
 
@@ -70,19 +71,26 @@ class TelegramController extends Controller
         $params = [
             'TgData' => $data
         ];
-        $res = $http->post('Telegram', ['form_params' => $params]);
-        return 'ok';
+        $http->postAsync('Telegram', ['form_params' => $params]);
     }
 
-    public function _sendMsg($data)
+    public function _send_msg($data)
     {
-//        $res = $request->all();
-//        $Info = $res['sendInfo'] ?? null;
-//        if (!$Info) return false;
-        $sendInfo = '我收到信息了！';
+        $sendInfo = ' |> 我收到信息了！';
         return Telegram::TelegramFun()->sendMessage([
             'chat_id' => $data['chat_id'],
             'text' => $data['text'] . $sendInfo,
+        ]);
+    }
+
+    public function _sendMsg(Request $request)
+    {
+        $res = $request->all();
+        if (!$res) return false;
+        $sendInfo = ' |> 我收到信息了！';
+        return Telegram::TelegramFun()->sendMessage([
+            'chat_id' => $res['chat_id'],
+            'text' => $res['text'] . $sendInfo,
         ]);
     }
 }
